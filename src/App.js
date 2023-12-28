15357096beb5481be1039f7c86cb7c45932843c2
anti-pc <buketculfaoglu@gmail.com>
2023-12-28 13:30:16 +0300

    import User

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import User from "./components/User"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <h1>test another component</h1>
       <h1>{1 + 5} - {"ankara".toUpperCase()}</h1>
       
+      <User></User>
+      <User></User>
+      <User></User>
+      <User></User>
 
     </div>
   );
