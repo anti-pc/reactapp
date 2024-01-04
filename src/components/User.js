bcc0867ddd2386b19181208bc9330364dd64bb2b
anti-pc <buketculfaoglu@gmail.com>
2024-01-04 18:21:02 +0300

    using setState

--- src/components/User.js
+++ src/components/User.js
@@ -13,18 +13,13 @@ import PropTypes from 'prop-types'
   }
 
 
-  onClickEvent = (number,e) => {
-    console.log(number);
-
+  onClickEvent = (e) => {
+    
+    //console.log(e.target)
+    this.setState({
+      isVisible : !this.state.isVisible
+    })
   }
-
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     isVisible : false
-  //   }
-  //}
 
   render() {
     
@@ -36,7 +31,7 @@ import PropTypes from 'prop-types'
       <div className='col-md-8 mb-4'>
         <div className='card'>
           <div className='card-header d-flex justify-content-between'>
-            <h4 className='d-inline' onClick={this.onClickEvent.bind(this, 34)}>{name}</h4>
+            <h4 className='d-inline' onClick={this.onClickEvent}>{name}</h4>
             <i className='far fa-trash-alt' style={{cursor:"pointer"}}></i>
           </div>
 
