4b2b701c91a3c4113dd48a1efeb8ba64b781000f
anti-pc <buketculfaoglu@gmail.com>
2024-01-04 16:23:39 +0300

    bootstrap card and font awesome used

--- src/components/User.js
+++ src/components/User.js
@@ -16,17 +16,26 @@ import PropTypes from 'prop-types'
     const {name,department,salary} = this.props;
 
     return (
-      <div>
-        <ul>
-            <li><i class="fa-regular fa-face-smile"></i> İsim : {name} </li> 
-            <li><i class="fa-solid fa-building"></i> Departman : {department} </li>
-            <li><i class="fa-solid fa-hand-holding-dollar"></i>Maaş : {salary} </li>
+      <div className='col-md-8 mb-4'>
+        <div className='card'>
+          <div className='card-header d-flex justify-content-between'>
+            <h4 className='d-inline'>{name}</h4>
+            <i className='far fa-trash-alt' style={{cursor:"pointer"}}></i>
+          </div>
 
-        </ul>
+          <div className='card-body'>
+            <p className='card-text'><i class="fa-solid fa-building"></i> Departman : {department}</p>
+            <p className='card-text'><i class="fa-solid fa-hand-holding-dollar"></i> Maaş : {salary}</p>            
+
+          </div>
+        </div>
       </div>
     )
   }
 }
+
+
+
 
 User.propTypes = {
   name : PropTypes.string.isRequired,
