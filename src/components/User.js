e8d3fb1f116e7ad953bcf6bbe4fd3b82ab019025
anti-pc <buketculfaoglu@gmail.com>
2024-01-04 18:04:02 +0300

    onclickEvent bind vers1

--- src/components/User.js
+++ src/components/User.js
@@ -35,7 +35,7 @@ import PropTypes from 'prop-types'
       <div className='col-md-8 mb-4'>
         <div className='card'>
           <div className='card-header d-flex justify-content-between'>
-            <h4 className='d-inline' onClick={this.onClickEvent}>{name}</h4>
+            <h4 className='d-inline' onClick={this.onClickEvent.bind(this)}>{name}</h4>
             <i className='far fa-trash-alt' style={{cursor:"pointer"}}></i>
           </div>
 
