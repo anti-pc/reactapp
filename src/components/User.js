4986887473e521b1fc837b2800cf2f1aa16c2968
anti-pc <buketculfaoglu@gmail.com>
2024-01-09 22:23:00 +0300

    Using Action, Reducer and Dispatch

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import UserConsumer from '../context'
 
 
  class User extends Component {
@@ -21,9 +22,11 @@ import PropTypes from 'prop-types'
     })
   }
 
-  onDeleteUser = (e) => {
-    //const{id} = this.props;
+  onDeleteUser = (dispatch,e) => {
+    const{id} = this.props;
     // Consumer dispatch
+
+    dispatch({type: "DELETE_USER", payload:id});
   }
 
   render() {
@@ -31,24 +34,56 @@ import PropTypes from 'prop-types'
 
     const {name,department,salary} = this.props;
     const {isVisible} = this.state;
-    return (
-      <div className='col-md-8 mb-4'>
-        <div className='card'>
-          <div className='card-header d-flex justify-content-between'>
-            <h4 className='d-inline' onClick={this.onClickEvent}>{name}</h4>
-            <i onClick={this.onDeleteUser} className='far fa-trash-alt' style={{cursor:"pointer"}}></i>
-          </div>
 
-          {
-            isVisible ? 
-            <div className='card-body'>
-              <p className='card-text'><i className="fa-solid fa-building"></i> Departman : {department}</p>
-              <p className='card-text'><i className="fa-solid fa-hand-holding-dollar"></i> Maaş : {salary}</p>
-            </div> : null
-          }
-        </div>
-      </div>
+    return (
+      <UserConsumer>
+      {
+        value => {
+          const {dispatch} = value;
+
+          return (
+            <div className='col-md-8 mb-4'>
+              <div className='card'>
+                <div className='card-header d-flex justify-content-between'>
+                  <h4 className='d-inline' onClick={this.onClickEvent}>{name}</h4>
+                  <i onClick={this.onDeleteUser.bind(this,dispatch)} className='far fa-trash-alt' style={{cursor:"pointer"}}></i>
+                </div>
+
+                {
+                  isVisible ? 
+                  <div className='card-body'>
+                    <p className='card-text'><i className="fa-solid fa-building"></i> Departman : {department}</p>
+                    <p className='card-text'><i className="fa-solid fa-hand-holding-dollar"></i> Maaş : {salary}</p>
+                  </div> : null
+                }
+              </div>
+            </div>
+          )
+
+        }
+      }
+      </UserConsumer>
     )
+
+    // return (
+    //   <div className='col-md-8 mb-4'>
+    //     <div className='card'>
+    //       <div className='card-header d-flex justify-content-between'>
+    //         <h4 className='d-inline' onClick={this.onClickEvent}>{name}</h4>
+    //         <i onClick={this.onDeleteUser} className='far fa-trash-alt' style={{cursor:"pointer"}}></i>
+    //       </div>
+
+    //       {
+    //         isVisible ? 
+    //         <div className='card-body'>
+    //           <p className='card-text'><i className="fa-solid fa-building"></i> Departman : {department}</p>
+    //           <p className='card-text'><i className="fa-solid fa-hand-holding-dollar"></i> Maaş : {salary}</p>
+    //         </div> : null
+    //       }
+    //     </div>
+    //   </div>
+    // )
+
   }
 }
 
